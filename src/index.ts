7f6abe0ecbbd9ce9081fc107b3ed4ce500704cfc
M. Fieldins <al285854@uji.es>
2024-05-31 21:43:11 +0200

    Avocado sample scene

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 // We define the empty imports so the auto-complete feature works as expected.
 import { Vector3 } from '@dcl/sdk/math'
-import { GltfContainer, Transform, engine } from '@dcl/sdk/ecs'
+import { GltfContainer, InputAction, Transform, engine, pointerEventsSystem } from '@dcl/sdk/ecs'
 
 import { changeColorSystem, circularSystem } from './systems'
 import { setupUi } from './ui'
@@ -22,4 +22,15 @@ export function main() {
   Transform.create(avocado, {
 		position: Vector3.create(3, 1, 3),
 	})
+
+  pointerEventsSystem.onPointerDown(
+    {
+      entity: avocado,
+      opts: { button: InputAction.IA_POINTER, hoverText: 'Collect' },
+    },
+    function () {
+      console.log('CLICKED AVOCADO')
+      engine.removeEntity(avocado)
+    }
+  )  
 }
